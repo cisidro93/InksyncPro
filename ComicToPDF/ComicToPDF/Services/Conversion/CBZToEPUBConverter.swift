58c94e3778337ffd5ce983db20592b44ef2e7516
chris <chrisisidro93@gmail.com>
2026-02-17 21:37:03 -0600

    Remove redundant xmlEscaped extension in CBZToEPUBConverter

--- ComicToPDF/ComicToPDF/Services/Conversion/CBZToEPUBConverter.swift
+++ ComicToPDF/ComicToPDF/Services/Conversion/CBZToEPUBConverter.swift
@@ -547,13 +547,3 @@ class CBZToEPUBConverter {
     }
 }
 
-extension String {
-    func xmlEscaped() -> String {
-        return self
-            .replacingOccurrences(of: "&", with: "&amp;")
-            .replacingOccurrences(of: "<", with: "&lt;")
-            .replacingOccurrences(of: ">", with: "&gt;")
-            .replacingOccurrences(of: "\"", with: "&quot;")
-            .replacingOccurrences(of: "'", with: "&apos;")
-    }
-}
